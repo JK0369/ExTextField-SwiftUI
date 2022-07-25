55fa5c35a894957a927f3590359f11a70dc5659c
jake.k <jake.k@hpcnt.com>
2022-07-25 16:57:36 +0900

    Initial Commit

---
+++ ExTextField/ExTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExTextFieldApp.swift
+//  ExTextField
+//
+//  Created by Jake.K on 2022/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ExTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
